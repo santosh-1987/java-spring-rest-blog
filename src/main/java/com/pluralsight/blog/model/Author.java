bc7772f93b732e82b69a63a79a0e01425172d1b7
Santosh Mohanty <Santosh.Mohanty@cerner.com>
2021-01-27 20:05:20 +0530

    Completed module 2

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -18,11 +18,12 @@ public class Author {
     private Long id;
     private String firstname;
     private String lastname;
-    private String username;
-    private String password;
+    @JsonIgnore private String username;
+    @JsonIgnore private String password;
 
     public Author() {
         super();
+        this.posts =  new ArrayList<>();
     }
 
     public Author(String username, String firstname, String lastname, String password) {
@@ -69,6 +70,8 @@ public class Author {
         return password;
     }
 
+    @OneToMany
+    private List<Post> posts;
     @Override
     public boolean equals(Object obj) {
         Author inputAuthor = (Author)obj;
@@ -85,10 +88,10 @@ public class Author {
     }
 
     public List<Post> getPosts() {
-        return null;
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+        posts.add(post);
     }
 }
